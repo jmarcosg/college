f3cecf6641ad288363192947b45b6fbf5fe74163
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2020-09-28 16:22:03 -0300

    EJ14TP5 corregido de acuerdo al enunciado

--- TDW/1/IP/TPs/5/ejercicio14.php
+++ TDW/1/IP/TPs/5/ejercicio14.php
@@ -25,41 +25,18 @@ function verificarValores ($h, $m, $s) {
 }
 
 /**
- * Esta función convierte la cantidad de horas ingresadas a formato 24hs
- * Para convertirla, el tipo de horario tiene que ser PM
- * La conversion es tal que: 1-13, 2-14, 3-15, ..., 12-24
- * @param int $hora
- * @return resultado
+ * Esta funcion verifica dos cantidades de segundos que recibe por parametro para ver cual es menor
+ * @param int $segs1
+ * @param int $segs2
+ * @return boolean esMenor
  */
-function convertirHora ($hora) {
-    if ($hora == 1) {
-        $hora = 13;
-    } elseif ($hora == 2) {
-        $hora = 14;
-    } elseif ($hora == 3) {
-        $hora = 15;
-    } elseif ($hora == 4) {
-        $hora = 16;
-    } elseif ($hora == 5) {
-        $hora = 17;
-    } elseif ($hora == 6) {
-        $hora = 18;
-    } elseif ($hora == 7) {
-        $hora = 19;
-    } elseif ($hora == 8) {
-        $hora = 20;
-    } elseif ($hora == 9) {
-        $hora = 21;
-    } elseif ($hora == 10) {
-        $hora = 22;
-    } elseif ($hora == 11) {
-        $hora = 23;
-    } elseif ($hora == 12) {
-        $hora = 24;
+function esMenor ($segs1, $segs2) {
+    if ($segs1 < $segs2) {
+        $esMenor = true;
     } else {
-        echo "ERROR: Hora incorrecta!";
+        $esMenor = false;
     }
-    return $hora;
+    return $esMenor;
 }
 
 /**
@@ -68,9 +45,44 @@ function convertirHora ($hora) {
  * @param int $hora
  * @param int $min
  * @param int $seg
+ * @param string $tipoH
  * @return int $segTotales
  */
-function convertirASegundos ($hora, $min, $seg) {
+function aSegundos ($hora, $min, $seg, $tipoH) {
+    // int $segTotales
+    // Verifico el tipo de horario para ver si es necesario convertir la hora
+    if ($tipoH == "PM") {
+        if ($hora == 1) {
+            $hora = 13;
+        } elseif ($hora == 2) {
+            $hora = 14;
+        } elseif ($hora == 3) {
+            $hora = 15;
+        } elseif ($hora == 4) {
+            $hora = 16;
+        } elseif ($hora == 5) {
+            $hora = 17;
+        } elseif ($hora == 6) {
+            $hora = 18;
+        } elseif ($hora == 7) {
+            $hora = 19;
+        } elseif ($hora == 8) {
+            $hora = 20;
+        } elseif ($hora == 9) {
+            $hora = 21;
+        } elseif ($hora == 10) {
+            $hora = 22;
+        } elseif ($hora == 11) {
+            $hora = 23;
+        } elseif ($hora == 12) {
+            $hora = 24;
+        } else {
+            echo "ERROR: Hora incorrecta!";
+        }
+    } else {
+        echo "Horas verificadas. No hace falta convertir."."\n";
+    }
+
     $segTotales = ($hora * 3600) + ($min * 60) + $seg;
     return $segTotales;
 }
@@ -79,26 +91,19 @@ function convertirASegundos ($hora, $min, $seg) {
  * Esta funcion calcula la diferencia horaria entre las horas ingresadas basado en la cantidad que representan en segundos
  * @param int $segs1
  * @param int $segs2
- * @return int $diferencia
  */
-function calcularDiferenciaHoraria($segs1, $segs2) {
+function difHoras($segs1, $segs2) {
+    // int $horas, $minutos, $segundos, $resto, $diferencia
+    // float $cociente
+    // Verifico cual de las dos horas en segundos es mayor para asi poder calcular la diferencia entre ambas
     if ($segs1 > $segs2) {
         $diferencia = $segs1 - $segs2; 
     } else {
         $diferencia = $segs2 - $segs1;
     }
-    return $diferencia;
-}
-
-/**
- * Esta funcion convierte una cantidad de segundos a horas, minutos y segundos
- * @param int $segs
- */
-function convertirSegundosHMS ($segs) {
-    // int $horas, $minutos, $segundos, $resto
-    // float $cociente
-    $cociente = (int)$segs / 60;
-    $resto = $segs % 60; // Segundos
+    // Calculo y convierto la diferencia de segundos a formato hh:mm:ss
+    $cociente = (int)$diferencia / 60;
+    $resto = $diferencia % 60; // Segundos
     $segundos = $resto;
 
     if ($cociente >= 60) {
@@ -113,11 +118,34 @@ function convertirSegundosHMS ($segs) {
 }
 
 /**
- * Este programa lee dos horas, minutos, segundos y tipo de horario diferentes
- * Luego las ordena de mayor a menor, muestra la cantidad de segundo que representan y la diferencia horaria
+ * Esta funcion convierte una cantidad de segundos a horas, minutos y segundos
+ * @param int $segsTotales
+ */
+function formatoHora ($segsTotales) {
+    // int $horas, $minutos, $segundos, $resto
+    // float $cociente
+    // Calculo y convierto los segundos totales a formato hh:mm:ss
+    $cociente = (int)$segsTotales / 60;
+    $resto = $segsTotales % 60; // Segundos
+    $segundos = $resto;
+
+    if ($cociente >= 60) {
+        $horas = (int)$cociente / 60;
+        $minutos = $cociente % 60;
+    } else {
+        $horas = (int)$cociente;
+        $minutos = $cociente % 60;
+    }
+
+    echo (int)$horas."h:".$minutos."m:".$segundos."s son ".$segsTotales. " seg."."\n";
+}
+
+/**
+ * Este programa lee dos horas, minutos, segundos y tipos de horarios diferentes
+ * Luego las ordena de mayor a menor, muestra la cantidad de segundos que representan y la diferencia horaria
  * int $hora1, $hora2, $min1, $min2, $seg1, $seg2, $cantS1, $cantS2, $difHoraria
  * string $tipoH1, $tipoH2
- * boolean $valoresCorrectos1, $valoresCorrectos2
+ * boolean $valoresCorrectos1, $valoresCorrectos2, $hora1Menor
 */
 
 // Ingreso y lectura de valores
@@ -145,35 +173,28 @@ $valoresCorrectos2 = verificarValores($hora2, $min2, $seg2);
 
 // Si los valores ingresados son correctos el programa continua, de lo contrario se corta
 if ($valoresCorrectos1 == true && $valoresCorrectos2 == true) {
-    // Verifico la hora y el tipo de horario para ver si es necesario convertirla
-    if ($tipoH1 == "PM") {
-        $hora1 = convertirHora($hora1);
-    } elseif ($tipoH2 == "PM") {
-        $hora2 = convertirHora($hora2);
-    } else {
-        echo "Horas verificadas. No hace falta convertir."."\n";
-    }
-
     // Convierto las horas, minutos y segundos ingresados a un total de segundos
-    $cantS1 = convertirASegundos($hora1, $min1, $seg1);
-    $cantS2 = convertirASegundos($hora2, $min2, $seg2);
+    $cantS1 = aSegundos($hora1, $min1, $seg1, $tipoH1);
+    $cantS2 = aSegundos($hora2, $min2, $seg2, $tipoH2);
 
-    // Verifico cual de las dos horas es mayor segun la cantidad de segundos
-    if ($cantS1 > $cantS2) {
-        echo $hora1."h:".$min1."m:".$seg1."s son ".$cantS1. " seg."."\n";
-        echo $hora2."h:".$min2."m:".$seg2."s son ".$cantS2. " seg."."\n";
+    // Invoco a la funcion para verficar cual de las dos cantidades de segundos es menor
+    $hora1Menor = esMenor($cantS1, $cantS2);
+
+    // Muestro cual hora es mayor segun el resultado anterior
+    if ($hora1Menor) {
+        echo "Las horas ordenadas de mayor a menor son: "."\n";
+        formatoHora($cantS2);
+        formatoHora($cantS1);
     } else {
-        echo $hora2."h:".$min2."m:".$seg2."s son ".$cantS2. " seg."."\n";
-        echo $hora1."h:".$min1."m:".$seg1."s son ".$cantS1. " seg."."\n";
+        echo "Las horas ordenadas de mayor a menor son: "."\n";
+        formatoHora($cantS1);
+        formatoHora($cantS2);
     }
 
-    // Invoco a la funcion para calcular la diferencia de tiempo entre las horas ingresadas y almaceno su valor
-    $difHoraria = calcularDiferenciaHoraria($cantS1, $cantS2);
-    // Al valor en segundos de la diferencia horaria lo transformo a formato hh:mm:ss 
-    convertirSegundosHMS($difHoraria);
+    // Invoco a la funcion para calcular la diferencia de tiempo entre las horas ingresadas 
+    difHoras($cantS1, $cantS2);
 } else {
     echo "ERROR: Valores incorrectos!";
 }
-
 
  ?>
