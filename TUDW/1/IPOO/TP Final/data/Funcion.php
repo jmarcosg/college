38ff200f068af86434af4728cdfa7e65a9d1b326
Juan Marcos Gonzalez <juan.gonzalez@est.fi.uncoma.edu.ar>
2021-06-12 22:35:11 -0300

    Archivos iniciales TPF IPOO

---
+++ TUDW/1/IPOO/TP Final/data/Funcion.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'BaseDatos.php';
+
+class Funcion
+{
+    private $id;
+    private $nombre;
+    private $horarioInicio;
+    private $duracion;
+    private $precio;
+    private $tipo;
+    private $idTeatro;
+
+    // Constructor
+    public function __construct()
+    {
+
+    }
+}
